2fd721c74b4e55983b6eba2adc3b7b71dbfede8d
turusko <turusko@gmail.com>
2025-02-08 14:06:46 +0000

    EOL

--- main.py
+++ main.py
@@ -1,20 +1,57 @@
 import pygame
+from obj.fighter import Fighter
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
+bottom_panel = 150
 screen_width = 800
-screen_height = 600
+screen_height = 400 + bottom_panel
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+
 pygame.display.set_caption("Battle Game")
+
+background = pygame.image.load("img/Background/background.png")
+
+panel_img = pygame.image.load("img/Icons/panel.png")
+
+def draw_bg():
+    screen.blit(background, (0, 0))
+
+def draw_pannel():
+    screen.blit(panel_img, (0, screen_height - bottom_panel))
+
+
+knight = Fighter(200, 260, "Knight", 30, 10, 3)
+bandit1 = Fighter(550, 270, "Bandit", 20, 6, 1)
+bandit2 = Fighter(700, 270, "Bandit", 20, 6, 1)
+
+bandit_list = [bandit1, bandit2]
+
+
 
 run = True
 
 while run:
-    
+    clock.tick(fps)
+
+    draw_bg()
+    draw_pannel()
+    knight.update()
+    knight.draw(screen)
+
+    for bandit in bandit_list: 
+        bandit.update()
+        bandit.draw(screen)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+            
 
-    screen.fill((0, 0, 0))
-    pygame.display.flip()
+    pygame.display.update()
+
+pygame.quit()
---
+++ obj/fighter.py
@@ -0,0 +1,49 @@
+import pygame
+import os
+
+
+class Fighter:
+    def __init__(self, x, y, name, max_hp, strength, potions):
+        self.name = name
+        self.max_hp = max_hp
+        self.hp = max_hp
+        self.strength = strength
+        self.start_potions = potions
+        self.potions = potions
+        self.alive = True
+        self.frame_index = 0
+        self.action = 0
+
+        self.update_time = pygame.time.get_ticks()
+        self.animation_list_types = ["Idle", "Attack", "Hurt", "Death"]
+        self.animation_list = self.load_animation()
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def load_animation(self):
+        temp_master_list = []
+        for animation_type in self.animation_list_types:
+            temp_list = []
+            num_of_frames = len(os.listdir(f"img/{self.name}/{animation_type}"))
+
+            for i in range(num_of_frames):
+                img = pygame.image.load(f"img/{self.name}/{animation_type}/{i}.png")
+                img = pygame.transform.scale(img, (img.get_width()*3, img.get_height()*3))
+                temp_list.append(img)
+            temp_master_list.append(temp_list)
+        return temp_master_list
+
+    def update(self, action="Idle"):
+        animation_cooldown = 100
+        self.action = self.animation_list_types.index(action)
+        self.image = self.animation_list[self.action][self.frame_index]
+
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            if self.frame_index >= len(self.animation_list):
+                self.frame_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
